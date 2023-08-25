0cae0755a269dab4dc20fc3fae840d5091241d4a
Rahul Kumar <kumarrahul771996@gmail.com>
2023-08-25 18:12:22 +0000

    binary_search done

--- binary_search.py
+++ binary_search.py
@@ -1,18 +1,22 @@
+'''
+valu: 1 2 3 4 5 6 7 8
+indx: 0 1 2 3 4 5 6 7
+'''
+
 arr = [1,2,3,4,5,6,7,8]
 to_be_searched = 5
 left = 0
-right = 0 
-
-while arr[left] < arr[right]:
-    mid = (left + right) // 2
-    for i in range(0, len(arr)):
-        if to_be_searched == arr[mid]:
-            print(i)
-
-        if to_be_searched < arr[mid]:
-            right = mid - 1
-        elif to_be_searched > arr[mid]:
-            left = mid + 1
+right = len(arr) - 1 #7
 
 
-        
+while arr[left] <= arr[right]:
+    mid = (left + right) // 2 # 5
+    if to_be_searched == arr[mid]:
+        print(mid+1)
+        break
+
+    if to_be_searched < arr[mid]:
+        right = mid - 1
+
+    elif to_be_searched > arr[mid]:
+        left = mid + 1
