edd689f7402c99e2b2ac70b27c6e6a5563dda585
Rahul Kumar <kumarrahul771996@gmail.com>
2024-02-04 23:09:43 +0530

    chore: add mock interview questions

--- oncampus/mockInterview.py
+++ oncampus/mockInterview.py
@@ -1,1 +1,12 @@
-print("hello world")
+# Questions, unable to answer
+
+# - How many sorting algorithms are there?
+
+''' 
+Create a class with a constructor which initializes a limit variable.
+create a method to print limit(say. 10) numbers, 
+whenever the method is called it should print 10 numbers
+eg. first call => print 1 to 10
+    second call => print 11 to 20
+'''
+
